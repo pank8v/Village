88aeb9cb2095dba000d84b31422dbccdaac6a7e8
pank8v <pank8v@gmail.com>
2025-10-24 18:55:58 +0300

    Flashlight

---
+++ Assets/Scripts/ItemS/FlashLight.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class FlashLight : MonoBehaviour, IItem
+{
+    [SerializeField] private Light light;
+    public GameObject ItemGameObject => gameObject;
+
+    private bool isActive;
+    
+    private void Awake() {
+        light.enabled = false;
+    }
+    public void Use(Transform _) {
+        isActive = !isActive;
+        light.enabled = isActive;
+    }
+    
+}
